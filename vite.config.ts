fdb542482f395133f35f119b1cdeaf217f14ec78
shahaji8848 <155730078+shahaji8848@users.noreply.github.com>
2025-03-18 12:27:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: 3000, // Ensure it's running on the correct port
+    strictPort: true,
+    allowedHosts: ['vendor-portal.8848digitalcloud.com'], // Allow your domain
+  },
 })
+
